25c31aad7d45542103dd97cfd09b411f5f6d49e9
abhinavv-as20 <66417041+abhinavv-as20@users.noreply.github.com>
2022-06-12 22:36:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,73 +1,114 @@
+//jshint esversion: 6
 //require of the dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash =require("lodash");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const logoutContent="Your are log-out of the system !!!"
-
-//setting-up ejs as view engine
 app.set('view engine', 'ejs');
-
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-//setting-up css files
-app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-//response of root route
-app.get("/",function(req,res){
-    console.log("home page");
+//response of registering and loging in of a user
+const userSchema ={
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/", function(req, res){
     res.render("home");
 });
 
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("homescreen");
+        }
+    });
+});
+
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        } else{
+            if(foundUser){
+                if(foundUser.password === password){
+                res.render("homescreen");
+                }
+            }
+        }
+    });
+});
+
 //response of "/about" route
+app.get("/homescreen", function(req, res){
+    res.render("homescreen");
+});
+
 app.get("/about",function(req,res){
-    console.log("about page");
     res.render("about");
 });
 
 //response of "/contact" route
 app.get("/contact",function(req,res){
-    console.log("contact page");
     res.render("contact");
 });
 
 //response of "/exam" route
 app.get("/exam",function(req,res){
-    console.log("exam page");
     res.render("exam");
 });
 
 //response of "/result" route
 app.get("/result",function(req,res){
-    console.log("result page");
     res.render("result");
 });
 
 // response to "/query" route
 app.get("/query",function(req,res){
-    console.log("query page");
     res.render("query");
 });
 
 // response to "/reset-password" route
 app.get("/reset-password",function(req,res){
-    console.log("reset-password page");
     res.render("reset-password");
 });
 
 // response to "/profile" route
 app.get("/profile",function(req,res){
-    console.log("profile page");
     res.render("profile");
 });
 
 // response to "/exam /test" route
-app.get("/exam/test",function(req,res){
-    console.log("test page");
-    res.render("test");
+app.get("/exam/test1",function(req,res){
+    res.render("test1");
 });
 
 // response to "/exam/thank" route
