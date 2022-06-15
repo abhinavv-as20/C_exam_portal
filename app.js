16354d9f5038f0a8cba4d0e25d8bd66032a6c2a4
Leena Madhok <87566353+LeenaMadhok@users.noreply.github.com>
2022-06-15 22:18:53 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-
+const logoutContent="Your are log-out of the system !!!"
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 //response of registering and loging in of a user
@@ -19,6 +19,41 @@ const userSchema ={
     email: String,
     password: String
 };
+
+//adding question to db by teacher interface
+const questionSchema={
+  subject_name:String,
+  ques:String,
+  option1:String,
+  option2:String,
+  option3:String,
+  option4:String,
+  correct_ans:String,
+  marks:Number
+};
+//model or collection
+const Question = mongoose.model("Question",questionSchema);
+app.post("/teacher/question-paper",function(req,res){
+  console.log(req.body);
+  const question = new Question({
+    subject_name :req.body.subject_name,
+    ques :req.body.newQuestion,
+    option1:req.body.option1,
+    option2:req.body.option2,
+    option3:req.body.option3,
+    option4:req.body.option4,
+    correct_ans:req.body.correct_ans,
+    marks:req.body.marks
+  });
+  question.save(function(err){
+    if(!err){
+      console.log("added");
+      res.redirect("/teacher/question-paper");
+    }else{
+      console.log(err);
+    }
+  });
+});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -69,45 +104,54 @@ app.post("/login", function(req, res){
 
 //response of "/about" route
 app.get("/homescreen", function(req, res){
+  console.log("homescreen page");
     res.render("homescreen");
 });
 
 app.get("/about",function(req,res){
+  console.log("about page");
     res.render("about");
 });
 
 //response of "/contact" route
 app.get("/contact",function(req,res){
+  console.log("contact page");
     res.render("contact");
 });
 
 //response of "/exam" route
 app.get("/exam",function(req,res){
+  console.log("exam page");
     res.render("exam");
 });
 
 //response of "/result" route
 app.get("/result",function(req,res){
+  console.log("result page");
     res.render("result");
 });
 
 // response to "/query" route
 app.get("/query",function(req,res){
+  console.log("query page.");
     res.render("query");
 });
 
 // response to "/reset-password" route
 app.get("/reset-password",function(req,res){
+  console.log("reset-password page.");
     res.render("reset-password");
 });
 
 // response to "/profile" route
 app.get("/profile",function(req,res){
+  console.log("profile page.");
     res.render("profile");
 });
 
 // response to "/exam /test" route
 app.get("/exam/test1",function(req,res){
+  console.log("question/test screen/page.");
     res.render("test1");
 });
 
@@ -123,6 +167,24 @@ app.get("/result/marks", function(req,res){
   res.render("marks");
 });
 
+// response to "/teacher" route
+app.get("/teacher", function(req,res){
+  console.log("Welcome to the teachers' side");
+  res.render("teacher");
+});
+
+// response to "/teacher/question-paper" route
+app.get("/teacher/question-paper", function(req,res){
+  console.log("to add question in db.");
+  res.render("question-paper");
+});
+
+// response to "/admin" route
+app.get("/admin", function(req,res){
+  console.log("Welcome to the admin's side");
+  res.render("admin");
+});
+
 //response of "/logout" route
 app.get("/logout",function(req,res){
     console.log("logout page");
@@ -130,6 +192,6 @@ app.get("/logout",function(req,res){
 });
 
 //port at localhost 3000.
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(8000, function() {
+  console.log("Server started on port 8000");
 });
